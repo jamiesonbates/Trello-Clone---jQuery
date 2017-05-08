f30e7ef50a6cd6458f056d2274c207deed852a60
Jamieson Bates <jamiesonbates@gmail.com>
2017-05-08 14:19:36 -0700

    Refactor population of lists via jQuery with new stylesheet and template

--- index.js
+++ index.js
@@ -2,34 +2,77 @@
   'use strict';
 
 // Read
-const tasks = ['Do laundry', 'Do my homework', 'Think of 5 project ideas', 'Read tomorrow\'s lecture', 'Find time to shower', 'Do 30 minutes of Code Wars'];
+const lists = [
+  {
+    title: 'My List',
+    tasks: ['Do your homework', 'Go to school', 'Eat dinner', 'Go to the gym', 'Write in journal']
+  },
+  {
+    title: 'Your List',
+    tasks: ['Go shopping', 'Go to soccer game']
+  }
+];
 
 const generateItem = function(value) {
-  const $divContainer = $('<div>').addClass('item');
+  const $itemDiv = $('<div>').addClass('item');
   const $li = $('<li>').text(value);
-  const $divBtn = $('<div>');
-  const $editBtn = $('<button>').text('Edit').addClass('edit-btn');
-  const $delBtn = $('<button>').text('Delete').addClass('del-btn');
+  const $btnContainer = $('<div>');
+  const $editBtn = $('<button>').addClass('btn edit-btn');
+  const $icon = $('<i>').addClass('fa fa-pencil');
 
-  $divContainer.append($li);
-  $divBtn.append($editBtn);
-  $divBtn.append($delBtn);
-  $divContainer.append($divBtn);
+  $itemDiv.append($li);
+  $editBtn.append($icon);
+  $btnContainer.append($editBtn);
+  $itemDiv.append($btnContainer);
 
-  return $divContainer;
+  return $itemDiv;
 }
 
-const populateList = function() {
-  const $list = $('ul');
+const createAddForm = function() {
+  const $toolsDiv = $('<div>').addClass('tools');
+  const $form = $('<form>');
+  const $input = $('<input>')
+    .attr('type', 'text')
+    .attr('placeholder', 'Add item')
+    .attr('autocomplete', 'off');
+  const $button = $('<button>')
+    .attr('type', 'submit')
+    .addClass('btn add-btn')
+    .text('Add');
 
-  for (const item of tasks) {
+  $form.append($input);
+  $form.append($button);
+  $toolsDiv.append($form);
+
+  return $toolsDiv;
+}
+
+const createAddPrompt = function() {
+  const $button = $('<button>').addClass('add-item').text('Add an item...');
+
+  return $button;
+}
+
+const createList = function(list) {
+  const $listDiv = $('<div>').addClass('list');
+  const $headerDiv = $('<div>').addClass('list-header');
+  const $h2 = $('<h2>').text(list.title);
+  const $list = $('<ul>');
+
+  $headerDiv.append($h2);
+  $listDiv.append($headerDiv);
+
+  for (const item of list.tasks) {
     const $divContainer = generateItem(item);
 
     $list.append($divContainer);
   }
-}
 
-populateList();
+  $listDiv.append($list);
+  $listDiv.append(createAddPrompt());
+
+  return $listDiv;
+}
 
 const generateForm = function(type, value) {
   const $form = $('<form>').attr('id', 'form-action');
@@ -46,6 +89,16 @@ const generateForm = function(type, value) {
   return $form;
 }
 
+const populateLists = function(lists) {
+  for (const list of lists) {
+    const $list = createList(list);
+
+    $('.all-lists').append($list);
+  }
+}
+
+populateLists(lists);
+
 // Update
 const startEdit = function($target) {
   const val = $target.children('li').text();
@@ -55,13 +108,6 @@ const startEdit = function($target) {
   $target.children('input').val(val);
   $target.children('div').children('button.edit-btn').text('Update');
   $target.addClass('editing');
-}
-
-const completeEdit = function($target, val) {
-  $target.removeClass('editing');
-  $target.children('div').children('button.edit-btn').text('Edit');
-  $target.children('input').remove();
-  $target.prepend($('<li>').text(val));
 }
 
 $('ul').on('click', '.edit-btn', (e) => {
@@ -100,6 +146,71 @@ $('#add-item').submit((e) => {
   $list.append($item);
   $('#item-to-add')[0].value = '';
   e.preventDefault();
+});
+
+const completeEdit = function($target, val) {
+  $target.removeClass('editing');
+  $target.children('div').children('button.edit-btn').text('Edit');
+  $target.children('input').remove();
+  $target.prepend($('<li>').text(val));
+}
+
+const inHover = function() {
+  if ($(this).hasClass('editing')) {
+    return;
+  }
+
+  const $button = $('<button>');
+  const $i = $('<i>');
+
+  $i.addClass('fa');
+  $i.addClass('fa-pencil');
+  $button.append($i);
+  $button.addClass('btn edit-btn');
+  $(this).append($button);
+}
+
+const outHover = function() {
+  $('.edit-btn').remove();
+}
+
+$('.item').hover(inHover, outHover);
+
+$('ul').on('click', '.edit-btn', (e) => {
+  let $button = $(e.target);
+
+  if ($button.hasClass('fa')) {
+    $button = $button.parents('button');
+  }
+
+  $button.parents('.item').addClass('editing');
+  $button.siblings('.hide').removeClass('hide');
+  $button.siblings('li').addClass('hide');
+  $('.edit-btn').remove();
+});
+
+$('button.add-item').on('click', (e) => {
+  $(e.target).addClass('hide');
+  $(e.target).siblings('form').removeClass('hide');
+});
+
+$('button#exit-edit-form').on('click', (e) => {
+  e.preventDefault();
+
+  const $item = $(e.target).parents('.item');
+
+  $item.removeClass('editing');
+  $item.children('li').removeClass('hide');
+  $item.children('form').addClass('hide');
+});
+
+$('button#exit-add-form').on('click', (e) => {
+  e.preventDefault();
+
+  const $tools = $(e.target).parents('.tools');
+
+  $tools.children('.add-item').removeClass('hide');
+  $tools.children('form').addClass('hide');
 });
 
 })();
